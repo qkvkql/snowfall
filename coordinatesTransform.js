28d34e190f0f02037fb7e07fabf6b7ceedd21140
qkvkql <15281061897@163.com>
2024-01-06 19:38:08 +0800

    remove reluctant comments

--- coordinatesTransform.js
+++ coordinatesTransform.js
@@ -48,39 +48,3 @@ function transformOneRow(str){
     newStr = arr[0] + ' ' + arr[1];
     return newStr;
 }
-
-
-/*
-function transform(str){
-    let newStr = '';
-    let patt = /\n/i;
-    let arr = str.split(patt);
-    for(let i=0; i<arr.length; i++){
-        newStr += transformLocation(arr[i]) + '\n';
-    }
-    return newStr;
-}
-
-function transformLocation(str){
-    let newStr = '';
-    let patt = /[\t ]/i;
-    let arr = str.split(patt);
-    if(arr.length !== 2){
-        return '输入格式错误';
-    }
-    if(arr[0].indexOf('-') !== -1){
-        arr[0] = arr[0].replace(/\-/i, '');
-        arr[0] = arr[0].trim() + '°S';
-    }else{
-        arr[0] = arr[0].trim() + '°N';
-    }
-    if(arr[1].indexOf('-') !== -1){
-        arr[1] = arr[1].replace(/\-/i, '');
-        arr[1] = arr[1].trim() + '°W';
-    }else{
-        arr[1] = arr[1].trim() + '°E';
-    }
-    newStr = arr[0] + ' ' + arr[1];
-    return newStr;
-}
-*/
