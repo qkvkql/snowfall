17df1c917e844b69cca7c90237fc9b8da6d0972a
qkvkql <15281061897@163.com>
2024-01-09 18:14:54 +0800

    innerHTML

--- GECardTable.js
+++ GECardTable.js
@@ -67,6 +67,8 @@ let colorArr = [
 ];
 
 function setValue(){
+    createCardDiv('resort-01');
+    document.getElementById('resort-01').innerHTML = cardTableHTML;
     for(let i=0; i<sfDataIdArr.length; i++){
         setOneValue(sfDataIdArr[i], sfDataIdArr, sfData);
     }
@@ -101,3 +103,125 @@ function setOneValue(id, idArr, dataArr){
         e.style.backgroundColor = colorArr[l];
     }
 }
+
+function createCardDiv(id){
+    let div = document.createElement('div');
+    div.setAttribute('id', id);
+    div.setAttribute('class', 'table-area');
+    document.body.appendChild(div);
+}
+
+let cardTableHTML = `<table>
+<!-- Basic Info -->
+<tr>
+    <td colspan="5" id="resort"><span class="resort-padding-r">雪乡龙岭野雪公园</span></td>
+</tr>
+<tr>
+    <td colspan="5" class="mountain">山脉：<span id="mountain">长白山</span></td>
+</tr>
+<tr>
+    <td colspan="5" class="country">国家：<span id="country">中国</span></td>
+</tr>
+<tr>
+    <td colspan="5" id="year" class="border-l border-r">2023年降雪量(mm)</td>
+</tr>
+
+<!-- SF -->
+<!-- Annual -->
+<tr>
+    <td class="total-label border-l border-r border-b">SF</td>
+    <td id="sf-total" class="border-r border-b"></td>
+    <td class="space"></td>
+    <td class="total-label border-l border-r border-b">SNO</td>
+    <td id="sno-total" class="border-r border-b"></td>
+</tr>
+<!-- Monthly -->
+<tr>
+    <td class="monthly-label border-l border-r">一月</td>
+    <td class="monthly-value border-r" id="sf-01"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">一月</td>
+    <td class="monthly-value border-r" id="sno-01"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">二月</td>
+    <td class="monthly-value border-r" id="sf-02"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">二月</td>
+    <td class="monthly-value border-r" id="sno-02"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">三月</td>
+    <td class="monthly-value border-r" id="sf-03"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">三月</td>
+    <td class="monthly-value border-r" id="sno-03"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">四月</td>
+    <td class="monthly-value border-r" id="sf-04"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">四月</td>
+    <td class="monthly-value border-r" id="sno-04"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">五月</td>
+    <td class="monthly-value border-r" id="sf-05"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">五月</td>
+    <td class="monthly-value border-r" id="sno-05"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">六月</td>
+    <td class="monthly-value border-r" id="sf-06"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">六月</td>
+    <td class="monthly-value border-r" id="sno-06"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">七月</td>
+    <td class="monthly-value border-r" id="sf-07"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">七月</td>
+    <td class="monthly-value border-r" id="sno-07"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">八月</td>
+    <td class="monthly-value border-r" id="sf-08"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">八月</td>
+    <td class="monthly-value border-r" id="sno-08"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">九月</td>
+    <td class="monthly-value border-r" id="sf-09"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">九月</td>
+    <td class="monthly-value border-r" id="sno-09"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">十月</td>
+    <td class="monthly-value border-r" id="sf-10"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">十月</td>
+    <td class="monthly-value border-r" id="sno-10"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">十一月</td>
+    <td class="monthly-value border-r" id="sf-11"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">十一月</td>
+    <td class="monthly-value border-r" id="sno-11"></td>
+</tr>
+<tr>
+    <td class="monthly-label border-l border-r">十二月</td>
+    <td class="monthly-value border-r" id="sf-12"></td>
+    <td class="space"></td>
+    <td class="monthly-label border-l border-r">十二月</td>
+    <td class="monthly-value border-r" id="sno-12"></td>
+</tr>
+<tr>
+    <td colspan="5" id="note">数据通过气象查询软件"WheatA小麦芽"查询得到<br>SF 和 SNO 是两种不同规则下的降雪量，不能混淆</td>
+</tr>
+
+</table>`;
